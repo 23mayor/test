3a1d0942550e8a0f7382b23eab948ab365d74156
Майоров Андрей Артемьевич <a.a.maiorov@Med.Samgmu.host>
2024-02-06 16:53:19 +0400

    trying to add sorting

--- src/pages/Table.tsx
+++ src/pages/Table.tsx
@@ -23,7 +23,17 @@ export default function Table () {
 
   }, [])
 
-console.log(people);
+  // let sortedPeople = people;
+
+  // function compare(a, b) {
+  //   if (a.name < b.name) {
+  //     return -1;
+  //   }
+  //   if (a.name > b.name) {
+  //     return -1;
+  //   }
+  //   return 0;
+  // }
 
   return (
 
